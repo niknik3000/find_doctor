5f445dee52e977a26c10ea309e699f65bb4739f3
volkovna <na.volkov@tensor.ru>
2022-11-21 11:00:52 +0300

    compose adapt

--- common_scripts/docker_useful_commands.sh
+++ common_scripts/docker_useful_commands.sh
@@ -1,8 +1,8 @@
-# чистка образов с тегом <none>
-docker images|grep none|awk '{print $3 }'|xargs docker rmi
-# чистка остановленных конейнеров
-docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
 # снести все контейнеры
 docker rm $(docker ps -a -q)
 # снести все образы
 docker rmi $(docker images -a -q)
+# чистка образов с тегом <none>
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
+# чистка остановленных конейнеров
+docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
