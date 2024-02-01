1aab9da3519851de9615d6a9d1542b58de7e4306
volkovna <na.volkov@tensor.ru>
2024-02-01 12:55:46 +0300

    123

--- build_compose.sh
+++ build_compose.sh
@@ -3,8 +3,8 @@ set -e
 sudo apt install jq yamllint -y
 jq . ./find_doctor/11347.json 1>/dev/null
 jq . ./find_doctor/30058.json 1>/dev/null
-python3 -m pip install --user yamllint
-yamllint ./find_doctor_compose.yaml
+# python3 -m pip install --user yamllint
+# yamllint ./find_doctor_compose.yaml
 docker-compose -f ./find_doctor_compose.yaml down
 cp ../common/common.py ./find_doctor/common.py
 docker-compose -f ./find_doctor_compose.yaml build --no-cache --parallel
